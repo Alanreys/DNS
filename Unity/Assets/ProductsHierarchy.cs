8cb28dfd34261bace117b6976ad3faed16ff7e2c
Alanreys <34255328+Alanreys@users.noreply.github.com>
2019-12-29 01:25:25 +1000

    bug fixed

---
+++ Unity/Assets/ProductsHierarchy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductsHierarchy : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Product")
+        {
+            collision.transform.parent = this.gameObject.transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Product")
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
